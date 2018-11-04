1db5b44ef714c16896517604c25d41829dcccb0f
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2018-11-04 14:57:36 -0700

    Cleanup and performance improvements? by explicit typing
    Especially specification of internal IndexablePoint2D in DelaunayTriangles and Edges

--- src/VoronoiDelaunayExtensions.jl
+++ src/VoronoiDelaunayExtensions.jl
@@ -7,12 +7,12 @@ export IndexablePoint2D
 mutable struct IndexablePoint2D <: AbstractPoint2D
     _x::Float64
     _y::Float64
-    _index::Int64
+    _index::Int
 end
 IndexablePoint2D(x::Float64, y::Float64) = IndexablePoint2D(x, y, -1)
 getx(p::IndexablePoint2D)::Float64 = p._x
 gety(p::IndexablePoint2D)::Float64 = p._y
-Base.getindex(p::IndexablePoint2D)::Int64 = p._index
-Base.setindex!(p::IndexablePoint2D, v::Int64) = setfield!(p, :_index, v)
+Base.getindex(p::IndexablePoint2D)::Int = p._index
+Base.setindex!(p::IndexablePoint2D, v::Int) = setfield!(p, :_index, v)
 Base.:+(p1::IndexablePoint2D, p2::IndexablePoint2D) = IndexablePoint2D(getx(p1)+getx(p2), gety(p1)+gety(p2), -1)
-Base.:/(p1::IndexablePoint2D, n::Number) = IndexablePoint2D(getx(p1)/n, gety(p1)/n, -1)
+Base.:/(p1::IndexablePoint2D, n::Real) = IndexablePoint2D(getx(p1)/n, gety(p1)/n, -1)
