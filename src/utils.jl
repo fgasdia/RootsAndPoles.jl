b598f56a8e52408020022c1cacf191af11e97fa4
fgasdia <fgasdia@noreply.github.com>
2020-05-24 14:50:02 -0600

    Remove unused functions

--- src/utils.jl
+++ src/utils.jl
@@ -5,10 +5,6 @@ distance((p1, p2)::Tuple{Complex, Complex}) = distance(p1, p2)
 function distance(p1::AbstractPoint2D, p2::AbstractPoint2D)
     return sqrt((getx(p2)-getx(p1))^2 + (gety(p2)-gety(p1))^2)
 end
-distance(e::DelaunayEdge) = distance(getb(e), geta(e))
-function distance(p1arr::AbstractArray{AbstractPoint2D}, p2::AbstractPoint2D)
-    return sqrt.((getx.(p1arr).-getx(p2)).^2 .+ (gety.(p1arr).-gety(p2)).^2)
-end
 
 """
     longedge(edge, tolerance, geom2fcn)
@@ -22,11 +18,7 @@ end
 """
     fcn2geom(z, ra, rb, ia, ib)
 
-Linearly map function values `z` within domain from `ra` to `rb` and `ia` to `ib`. Necessary
-because [VoronoiDelaunay.jl](https://github.com/JuliaGeometry/VoronoiDelaunay.jl) requires
-point coordinates be within `min_coord <= x <= max_coord` where `min_coord=1.0+eps(Float64)`
-and `max_coord=2.0-2eps(Float64)`. `max_coord` and `min_coord` are provided by
-`VoronoiDelaunay.jl`
+Linearly map function values `z` within domain from `ra` to `rb` and `ia` to `ib`.
 """
 function fcn2geom(z, ra, rb, ia, ib)
     zr = ra*real(z) + rb
@@ -39,14 +31,11 @@ end
 
 Linearly map geometry values ∈ {`min_coord`, `max_coord`} to domain bounds.
 
-Note: There are floating point errors when converting back and forth.
+Note: There might be floating point errors when converting back and forth.
 """
 function geom2fcn(pt::AbstractPoint2D, ra, rb, ia, ib)
     return complex((getx(pt) - rb)/ra, (gety(pt) - ib)/ia)
 end
-function geom2fcn(edge::VoronoiDelaunay.DelaunayEdge{P}, ra, rb, ia, ib) where P <: AbstractPoint2D
+function geom2fcn(edge::DelaunayEdge{P}, ra, rb, ia, ib) where P <: AbstractPoint2D
     return geom2fcn(geta(edge), ra, rb, ia, ib), geom2fcn(getb(edge), ra, rb, ia, ib)
-end
-function geom2fcn(x, y, ra, rb, ia, ib)
-    return complex((x-rb)/ra, (y-ib)/ia)
 end
