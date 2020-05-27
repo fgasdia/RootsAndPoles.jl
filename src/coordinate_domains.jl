8a7beb26dafbbc3fb6517d03e55facc3c3b3006f
fgasdia <fgasdia@noreply.github.com>
2020-05-26 18:33:07 -0600

    performance improvements to rectdomain and diskdomain complete

--- src/coordinate_domains.jl
+++ src/coordinate_domains.jl
@@ -3,93 +3,69 @@
 
 Generate initial mesh node coordinates for a rectangular domain ∈ {`Zb`, `Ze`} with
 initial mesh step `Δr`.
+
+`Ze` should be greater than `Zb`.
 """
 function rectangulardomain(Zb::Complex, Ze::Complex, Δr)
     rZb, iZb = reim(Zb)
     rZe, iZe = reim(Ze)
 
+    # todo: better error
+    rZb > rZe && throw(ArgumentError("real part of Zb greater than Ze"))
+    iZb > iZe && throw(ArgumentError("imag part of Zb greater than Ze"))
+
     X = rZe - rZb
     Y = iZe - iZb
 
     n = ceil(Int, Y/Δr + 1)
     dy = Y/(n-1)
+    half_dy = dy/2
+
     m = ceil(Int, X/sqrt(Δr^2 - dy^2/4) + 1)
     dx = X/(m-1)
 
-    vx = range(rZb, stop=rZe, length=m)
-    vy = range(iZb, stop=iZe, length=n)
+    # precalculate
+    mn = m*n
 
-    y = repeat(vy, m)
+    vlength = mn + div(m, 2)
+    v = Vector{promote_type(typeof(Zb), typeof(Ze), typeof(Δr))}(undef, vlength)
 
-    half_dy = dy/2
+    I = LinearIndices((n,m))
     on = false
-    for i in eachindex(y)
-        if i % n == 0
+    @inbounds for j in 0:m-1, i in 0:n-1
+        # dx/j and dy/i flipped from what would seem "natural" to match Matlab meshgrid
+        x = rZb + dx*j
+        y = iZb + dy*i
+
+        # `n` zeros then `n-1` ones and one zero, then repeat: `n` zeros, etc
+        if (i+1) % n == 0
             on = !on
-        elseif on
-            y[i] += half_dy
-        end
-    end
-
-    y = vcat(y, fill(iZb, div(m,2)))  # TODO: why?
-
-    x = repeat(vx, inner=n)
-    x = vcat(x, (1:2:(m-1))*dx .+ rZb)
-
-    return complex.(x, y)
-end
-
-
-function rectangulardomain_new(Zb::Complex, Ze::Complex, Δr)
-    rZb, iZb = reim(Zb)
-    rZe, iZe = reim(Ze)
-
-    X = rZe - rZb
-    Y = iZe - iZb
-
-    n = ceil(Int, Y/Δr + 1)
-    dy = Y/(n-1)
-    half_dy = dy/2
-
-    m = ceil(Int, X/sqrt(Δr^2 - dy^2/4) + 1)
-    dx = X/(m-1)
-
-    vlength = m*n + div(m, 2)
-    v = Vector{promote_type(typeof(Zb), typeof(Zb))}(undef, vlength)
-
-    I = LinearIndices(v)
-    @inbounds for j in 0:n-1, i in 0:m-1
-        x = rZb + dx*i
-        y = iZb + dy*j
-
-        on = false
-        if j % n == 0
-            on = !on
-        elseif on
+        elseif on && ((i+1) % n != 0)
             y += half_dy
         end
         v[I[i+1,j+1]] = complex(x, y)
     end
 
-    # The "extra" rows or columns still cover the whole range from `Zb` to `Ze`
-    # TODO: finish this part!
+    idx = mn
+    @inbounds for i in 1:2:(m-1)
+        idx += 1
 
-    y = vcat(y, fill(iZb, div(m,2)))
+        # The "extra" rows or columns still cover the whole range from `Zb` to `Ze`
+        tx = rZb + dx*i
+        ty = iZb
+        v[idx] = complex(tx, ty)
+    end
 
-    x = repeat(vx, inner=n)
-    x = vcat(x, (1:2:(m-1))*dx .+ rZb)
-
-    return complex.(x, y)
+    return v
 end
-
 
 """
     diskdomain(R, Δr)
 
-Generate initial mesh coordinates for a circular disk domain of radius `R` and center (0, 0)
-for ``|z| < R``.
+Generate initial mesh coordinates for a circular disk domain of radius `R` and
+center (0, 0) for ``|z| < R`` and initial mesh step `Δr`.
 """
-function diskdomain_new(R, Δr)
+function diskdomain(R, Δr)
     h = Δr*sqrt(3)/2
     n = 1 + round(Int, R/h)
     R_n = R/n
@@ -99,7 +75,7 @@ function diskdomain_new(R, Δr)
 
     f₀step = π/(6n)
 
-    newnodes = Vector{complex(typeof(R))}(undef, 6*sum(1:n)+1)
+    newnodes = Vector{complex(promote_type(typeof(R), typeof(Δr)))}(undef, 6*sum(1:n)+1)
     newnodes[1] = 0
 
     idx = 2
