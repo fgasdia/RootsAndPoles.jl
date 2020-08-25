f3f029df3c0dd6b723d89af22aee943e2189fba0
fgasdia <fgasdia@noreply.github.com>
2020-08-24 21:28:42 -0600

    very specific, limited triangulardomain

--- src/coordinate_domains.jl
+++ src/coordinate_domains.jl
@@ -10,7 +10,7 @@ function rectangulardomain(Zb::Complex, Ze::Complex, Δr)
     rZb, iZb = reim(Zb)
     rZe, iZe = reim(Ze)
 
-    # todo: better error
+    # TODO: Just redefine Zb and Ze...
     rZb > rZe && throw(ArgumentError("real part of Zb greater than Ze"))
     iZb > iZe && throw(ArgumentError("imag part of Zb greater than Ze"))
 
@@ -58,6 +58,106 @@ function rectangulardomain(Zb::Complex, Ze::Complex, Δr)
 
     return v
 end
+
+"""
+    triangulardomain(Za, Zb, Zc, Δr)
+
+Generate initial mesh node coordinates for a *grid-aligned right* triangle domain
+∈ {`Za`, `Zb`, `Zc`} with initial mesh step `Δr`.
+
+This function generates a mesh grid for particular right triangles where two
+sides of the triangle are aligned to the underlying real/imaginary grid. Examples
+of such triangles are:
+
+a ----- b
+|     /
+|   /
+| /
+c
+
+where
+    - a, b have greatest extent in x
+    - a, c have greatest extent in y
+"""
+function triangulardomain(Za::Complex, Zb::Complex, Zc::Complex, Δr)
+    rZa, iZa = reim(Za)
+    rZb, iZb = reim(Zb)
+    rZc, iZc = reim(Zc)
+
+    # Check if this is a right triangle
+    validtriangle = true
+    if rZa == rZb == rZc
+        validtriangle = false
+    elseif iZa == iZb == iZc
+        validtriangle = false
+    elseif rZa == rZb
+        iZa == iZc || iZb == iZc || (validtriangle = false)
+    elseif rZa == rZc
+        iZa == iZb || iZb == iZc || (validtriangle = false)
+    elseif rZb == rZc
+        iZa == iZb || iZa == iZc || (validtriangle = false)
+    else
+        validtriangle = false
+    end
+    validtriangle || throw(ArgumentError("`Za`, `Zb`, `Zc` do not define a grid-aligned right triangle"))
+
+    iZa == iZb || ((Zb, Zc) = (Zc, Zb))
+    rZb > rZa || ((Za, Zb) = (Zb, Za))
+
+    # Determine `dx` and `dy`
+    X = rZb - rZa
+    Y = abs(iZa - iZc)
+
+    n = ceil(Int, Y/Δr + 1)
+    dy = Y/(n-1)
+    half_dy = dy/2
+
+    m = ceil(Int, X/sqrt(Δr^2 - dy^2/4) + 1)
+    dx = X/(m-1)
+
+    # precalculate
+    mn = m*n
+    slope = Y/X
+
+    vlength = mn + div(m, 2)  # BUG?
+    T = promote_type(typeof(Za), typeof(Zb), typeof(Zc), typeof(Δr), Float64)
+    v = Vector{T}()
+
+    on = false
+    for j = 0:m-1  # col
+        for i = 0:n-1  # row (we're traversing down column)
+
+            x = rZa + dx*j
+            y = iZc + dy*i
+
+            if (i+1) == n
+                on = !on
+            end
+            if on
+                y -= half_dy
+            end
+
+            if y >= (iZc + slope*(x - rZa))
+                push!(v, complex(x, y))
+            end
+        end
+    end
+
+    return v
+end
+
+
+Za = complex(50, -0.1)
+Zb = complex(85, -0.1)
+Zc = complex(50, -6.0)
+r = 0.5
+
+v = triangulardomain(Za, Zb, Zc, r)
+
+plot(real(v),imag(v),"o")
+xlim(50,85)
+ylim(-6,0)
+
 
 """
     diskdomain(R, Δr)
