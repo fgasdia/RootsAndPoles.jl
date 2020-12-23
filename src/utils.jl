9e61548393848f5df43be83b1f54775d36c61323
fgasdia <17058025+fgasdia@users.noreply.github.com>
2020-12-22 22:11:07 -0700

    Add getplotdata convenience function.

--- src/utils.jl
+++ src/utils.jl
@@ -83,3 +83,57 @@ function geom2fcn(edge::DelaunayEdge{P}, rmin, rmax, imin, imax) where P <: Abst
     return (geom2fcn(geta(edge), rmin, rmax, imin, imax),
         geom2fcn(getb(edge), rmin, rmax, imin, imax))
 end
+
+"""
+    getplotdata(tess, quadrants, phasediffs, g2f)
+
+Return complex coordinates of every tesselation edge and a vector with the corresponding
+index specifiying its phase quadrant.
+
+This function is useful for plotting the final tesselation using results from calling
+`grpf` with the `PlotData()` argument.
+
+The edge colors are (where Q means "quadrant"):
+
+| Color index |         Meaning         |  Quadrant phase   |
+|:-----------:|:-----------------------:|:-----------------:|
+|      1      |           Q 1           | 0 ≤ arg f < π/2   |
+|      2      |           Q 2           | π/2 ≤ arg f < π   |
+|      3      |           Q 3           | π ≤ arg f < 3π/2  |
+|      4      |           Q 4           | 3π/2 ≤ arg f < 2π |
+|      5      | phase change (boundary) |         -         |
+|      6      | candidate edge          |         -         |
+
+# Example
+
+Although not self contained, this example shows how `getplotdata` can be used:
+
+```julia
+roots, poles, quads, phasediffs, tess, g2f = grpf(f, origcoords, PlotData());
+z, edgecolors = getplotdata(tess, quadrants, phasediffs, g2f);
+
+using Plots
+plot(real(z), imag(z), group=edgecolors)
+```
+"""
+function getplotdata(tess, quadrants, phasediffs, g2f)
+    edges = [e for e in delaunayedges(tess)]
+
+    edgecolors = fill(5, length(edges))
+    for ii in eachindex(edges)
+        if phasediffs[ii] == 2  # candidate edges
+            edgecolors[ii] = 6
+        elseif phasediffs[ii] == 0
+            edgecolors[ii] = quadrants[getindex(geta(edges[ii]))]
+        end
+    end
+
+    edgecolors = repeat(edgecolors, inner=3)  # x, y is xₐ, xᵦ, NaN, repeat
+
+    x, y = getplotxy(edges)
+    z = g2f.(x, y)
+
+    I = sortperm(edgecolors)
+
+    return z[I], edgecolors[I]
+end
